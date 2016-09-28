6b811709f2a3c124e14f2c5097f7a04c2b8bff4d
Jurjen Munneke <jurjenmunneke@me.com>
2016-09-28 17:12:09 +0200

    create first component

---
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -0,0 +1,17 @@
+var EventApplication = React.createClass({
+  render: function() {
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1>Ilumy Calendar</h1>
+          <p>by Jurjen Munneke</p>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <EventTable />
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
